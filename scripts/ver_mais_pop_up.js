1d3a37654cd55afbd79f6553fc25cfa2d7656b7a
kassike <kaikems27@outlook.com>
2023-09-12 10:47:54 -0300

    comecei os popups , falta centrlizar

---
+++ scripts/ver_mais_pop_up.js
@@ -0,0 +1,29 @@
+const botao_1 = document.querySelector("#botao_1")
+const popup_1 = document.querySelector("#popup1")
+const fechar_1 = document.querySelector("#fechar_1")
+
+const botao_2 = document.querySelector("#botao_2")
+const popup_2 = document.querySelector("#popup2")
+const fechar_2 = document.querySelector("#fechar_2")
+
+const botao_3 = document.querySelector("#botao_3")
+const popup_3 = document.querySelector("#popup3")
+const fechar_3 = document.querySelector("#fechar_3")
+
+const botao_4 = document.querySelector("#botao_4")
+const popup_4 = document.querySelector("#popup4")
+const fechar_4 = document.querySelector("#fechar_4")
+
+botao_1.onclick = function (){ popup_1.showModal() }
+fechar_1.onclick = function (){ popup_1.close() }
+
+botao_2.onclick = function (){ popup_2.showModal() }
+fechar_2.onclick = function () { popup_2.close() }
+
+botao_3.onclick = function (){ popup_3.showModal() }
+fechar_3.onclick = function () { popup_3.close() }
+
+botao_4.onclick = function (){ popup_4.showModal() }
+fechar_4.onclick = function () { popup_4.close() }
+
+
